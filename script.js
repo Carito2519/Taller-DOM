8575272cb252b555f8d8719ba8b01192d11a1f91
carolina leal <carola251933@gmail.com>
2022-11-22 18:22:11 -0500

    creacion de api propio

--- script.js
+++ script.js
@@ -2,17 +2,26 @@ const tituloEspecial = document.querySelector('#especial');
 const templateProducto = document.querySelector('#template-producto');
 const contenedorProductos = document.querySelector('.contenedor-productos');
 
+const apiPropio = {}
+
 function obtenerProductos() {
     fetch('https://api.escuelajs.co/api/v1/products')
         .then(respuesta => respuesta.json())
         .then(datos => {
-            datos.forEach(producto => {
+            apiPropio = datos.filter((_, index) => index < 10);
+
+            apiPropio.forEach(producto => {
                 const nuevoProducto = templateProducto.content.cloneNode(true);
                 nuevoProducto.querySelector('.titulo').textContent =
                     producto.title;
                 nuevoProducto.querySelector('.precio').textContent =
                     producto.price;
                 nuevoProducto.querySelector('.imagen').src = producto.images[2];
+                const boton = nuevoProducto.querySelector('.agregar-btn');
+
+                boton.addEventListener('click', () => {
+                    agregarCarrito(producto);
+                });
 
                 contenedorProductos.appendChild(nuevoProducto);
             });
