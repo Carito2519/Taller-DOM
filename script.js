0825bb3a199d97c95f6844d2cad346f3316f0675
carolina leal <carola251933@gmail.com>
2022-11-22 18:31:42 -0500

    refactorizar codigo

--- script.js
+++ script.js
@@ -9,23 +9,23 @@ function obtenerProductos() {
         .then(respuesta => respuesta.json())
         .then(datos => {
             apiPropio = datos.filter((_, index) => index < 10);
-
-            apiPropio.forEach(producto => {
-                const nuevoProducto = templateProducto.content.cloneNode(true);
-                nuevoProducto.querySelector('.titulo').textContent =
-                    producto.title;
-                nuevoProducto.querySelector('.precio').textContent =
-                    producto.price;
-                nuevoProducto.querySelector('.imagen').src = producto.images[2];
-                const boton = nuevoProducto.querySelector('.agregar-btn');
-
-                boton.addEventListener('click', () => {
-                    agregarCarrito(producto);
-                });
-
-                contenedorProductos.appendChild(nuevoProducto);
-            });
+            recargarProductos();
         });
 }
 
+function recargarProductos() {
+    apiPropio.forEach(producto => {
+        const nuevoProducto = templateProducto.content.cloneNode(true);
+        nuevoProducto.querySelector('.titulo').textContent = producto.title;
+        nuevoProducto.querySelector('.precio').textContent = producto.price;
+        nuevoProducto.querySelector('.imagen').src = producto.images[2];
+        const boton = nuevoProducto.querySelector('.agregar-btn');
+
+        boton.addEventListener('click', () => {
+            agregarCarrito(producto);
+        });
+
+        contenedorProductos.appendChild(nuevoProducto);
+    });
+}
 obtenerProductos();
