9094ab6384d2f41bb79f09513099839e4a77939b
carolina leal <carola251933@gmail.com>
2022-11-22 19:27:20 -0500

    funcionalidad de carrito

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 const tituloEspecial = document.querySelector('#especial');
 const templateProducto = document.querySelector('#template-producto');
+const templateCarrito = document.querySelector('#template-carrito');
 const contenedorProductos = document.querySelector('.contenedor-productos');
+const contenedorCarrito = document.querySelector('.contenedor-carrito');
 
-const apiPropio = {};
+const apiPropio = { carrito: [] };
 
 function obtenerProductos() {
     fetch('https://api.escuelajs.co/api/v1/products')
@@ -14,6 +16,8 @@ function obtenerProductos() {
 }
 
 function recargarProductos() {
+    contenedorProductos.innerHTML = '';
+
     apiPropio.productos.forEach(producto => {
         const nuevoProducto = templateProducto.content.cloneNode(true);
         nuevoProducto.querySelector('.titulo').textContent = producto.title;
@@ -28,4 +32,35 @@ function recargarProductos() {
         contenedorProductos.appendChild(nuevoProducto);
     });
 }
+
+function agregarCarrito(producto) {
+    const indice = apiPropio.carrito.findIndex(p => p.id == producto.id);
+
+    if (indice != -1) {
+        apiPropio.carrito[indice].cantidad += 1;
+    } else {
+        apiPropio.carrito.push({ id: producto.id, cantidad: 1 });
+    }
+    recargarCarrito();
+}
+
+function recargarCarrito() {
+    contenedorCarrito.innerHTML = '';
+
+    apiPropio.carrito.forEach(data => {
+        const producto = apiPropio.productos.filter(p => p.id == data.id)[0];
+
+        const nuevoCarrito = templateCarrito.content.cloneNode(true);
+        nuevoCarrito.querySelector('.titulo').textContent = producto.title;
+        nuevoCarrito.querySelector('.precio').textContent = producto.price;
+        nuevoCarrito.querySelector('.cantidad').textContent = data.cantidad;
+        nuevoCarrito.querySelector('.total').textContent =
+            producto.price * data.cantidad;
+        nuevoCarrito.querySelector('.imagen').src = producto.images[2];
+        const boton = nuevoCarrito.querySelector('.eliminar-btn');
+
+        contenedorCarrito.appendChild(nuevoCarrito);
+    });
+}
+
 obtenerProductos();
