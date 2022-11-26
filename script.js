ae3f46f1038fc4611b57c832d182be5ccab6f9a0
carolina leal <carola251933@gmail.com>
2022-11-25 19:18:19 -0500

    Editar y eliminar botones

--- script.js
+++ script.js
@@ -4,13 +4,14 @@ const templateCarrito = document.querySelector('#template-carrito');
 const contenedorProductos = document.querySelector('.contenedor-productos');
 const contenedorCarrito = document.querySelector('.contenedor-carrito');
 
-const apiPropio = { carrito: [] };
+const apiPropio = { productos: [], carrito: [] };
 
 function obtenerProductos() {
-    fetch('https://api.escuelajs.co/api/v1/products')
+    fetch('https://fakestoreapi.com/products')
         .then(respuesta => respuesta.json())
         .then(datos => {
             apiPropio.productos = datos.filter((_, index) => index < 10);
+            console.log(apiPropio.productos);
             recargarProductos();
         });
 }
@@ -22,11 +23,38 @@ function recargarProductos() {
         const nuevoProducto = templateProducto.content.cloneNode(true);
         nuevoProducto.querySelector('.titulo').textContent = producto.title;
         nuevoProducto.querySelector('.precio').textContent = producto.price;
-        nuevoProducto.querySelector('.imagen').src = producto.images[2];
-        const boton = nuevoProducto.querySelector('.agregar-btn');
+        nuevoProducto.querySelector('.imagen').src = producto.image;
+        const botonAgregar = nuevoProducto.querySelector('.agregar-btn');
+        const botonEliminar = nuevoProducto.querySelector('.eliminar-btn');
+        const botonEditar = nuevoProducto.querySelector('.editar-btn');
 
-        boton.addEventListener('click', () => {
+        botonAgregar.addEventListener('click', () => {
             agregarCarrito(producto);
+        });
+
+        botonEliminar.addEventListener('click', () => {
+            apiPropio.productos = apiPropio.productos.filter(
+                elem => elem.id != producto.id
+            );
+            recargarProductos();
+        });
+
+        botonEditar.addEventListener('click', () => {
+            const nuevoTitulo = prompt('Nuevo título:', producto.title);
+            const nuevoPrecio = prompt('Nuevo precio:', producto.price);
+
+            apiPropio.productos = apiPropio.productos.map(elem => {
+                if (elem.id == producto.id) {
+                    return {
+                        ...producto,
+                        title: nuevoTitulo,
+                        price: nuevoPrecio,
+                    };
+                } else {
+                    return elem;
+                }
+            });
+            recargarProductos();
         });
 
         contenedorProductos.appendChild(nuevoProducto);
@@ -56,8 +84,15 @@ function recargarCarrito() {
         nuevoCarrito.querySelector('.cantidad').textContent = data.cantidad;
         nuevoCarrito.querySelector('.total').textContent =
             producto.price * data.cantidad;
-        nuevoCarrito.querySelector('.imagen').src = producto.images[2];
+        nuevoCarrito.querySelector('.imagen').src = producto.image;
         const boton = nuevoCarrito.querySelector('.eliminar-btn');
+
+        boton.addEventListener('click', () => {
+            apiPropio.carrito = apiPropio.carrito.filter(
+                p => p.id != producto.id
+            );
+            recargarCarrito();
+        });
 
         contenedorCarrito.appendChild(nuevoCarrito);
     });
